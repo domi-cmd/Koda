f9e1881c3a7a0e9908d8212dcde5f3fbe8cc93db
domi-cmd <dokro@gmx.ch>
2026-07-02 11:33:00 +0200

    setup basic project structure, added template logic for recording audio via pyaudio

---
+++ src/logic/audio_recorder.py
@@ -0,0 +1,32 @@
+"""PyAudio Example: Record a few seconds of audio and save to a wave file."""
+
+import wave
+import sys
+import pyaudio
+
+class AudioRecorder:
+    CHUNK = 1024
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 1 if sys.platform == 'darwin' else 2
+    RATE = 44100
+    RECORD_SECONDS = 5
+
+    def __init__(self):
+        pass
+
+    def recordAudio(self):
+        with wave.open('output/output.wav', 'wb') as wf:
+            p = pyaudio.PyAudio()
+            wf.setnchannels(self.CHANNELS)
+            wf.setsampwidth(p.get_sample_size(self.FORMAT))
+            wf.setframerate(self.RATE)
+
+            stream = p.open(format=self.FORMAT, channels=self.CHANNELS, rate=self.RATE, input=True)
+
+            print('Recording...')
+            for _ in range(0, self.RATE // self.CHUNK * self.RECORD_SECONDS):
+                wf.writeframes(stream.read(self.CHUNK))
+            print('Done')
+
+            stream.close()
+            p.terminate()
---
+++ src/main.py
@@ -0,0 +1,9 @@
+from logic.audio_recorder import AudioRecorder
+
+def main():
+    audioRecorder = AudioRecorder()
+    audioRecorder.recordAudio()
+
+
+if __name__ == "__main__":
+    main()
